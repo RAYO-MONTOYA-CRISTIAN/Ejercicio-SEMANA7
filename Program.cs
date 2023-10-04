0be5b0c17982cacbdef423f6c2c6d07e369f91ae
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-10-03 22:24:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el varo n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i= 1; i <= n; i++) 
+            {
+                float resultado = (16 - i) / (3.0f * i);
+                Console.WriteLine($"Resultado {i}: {resultado:F2}");
+
+            }
+        }
+    }
+}
